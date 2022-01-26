ee6732b9fd43c9cf62d287ded542c01cd0ffd20b
katie <katie.dwyer@techelevator.com>
2022-01-26 17:25:58 -0500

    starter code for lecture

---
+++ module-1/09_Classes_and_Encapsulation/lecture/src/main/java/com/techelevator/MainProgram.java
@@ -0,0 +1,15 @@
+package com.techelevator;
+
+import java.util.Scanner;
+
+class MainProgram {
+
+    /**
+     * The main method is the start and end of our program
+     */
+    public static void main(String[] args) {
+
+
+    }
+
+}
