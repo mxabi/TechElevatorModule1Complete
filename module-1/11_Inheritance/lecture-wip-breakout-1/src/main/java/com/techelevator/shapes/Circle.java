dee0755352dd2f7d81459265e5ad112705dd58c5
katie <katie.dwyer@techelevator.com>
2022-01-31 10:25:33 -0500

    breakout 1 wip

---
+++ module-1/11_Inheritance/lecture-wip-breakout-1/src/main/java/com/techelevator/shapes/Circle.java
@@ -0,0 +1,13 @@
+package com.techelevator.shapes;
+
+public class Circle extends Shape{
+    //since Circle extends Shape, it inherits: color, filled, numLines, and all the methods
+
+    private double radius;
+    public double getRadius(){return radius;}
+    public void setRadius(double radius){this.radius = radius;}
+
+    //constrctor - set the radius, set the color, set filled, set num lines
+
+    //get perimeter - calculate and return the perimeter
+}
